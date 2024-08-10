1dc8592d0532510b75bd72f8049802080c65c59a
PabloKauan-TI <kpablo429@gmail.com>
2024-08-10 12:31:27 -0300

    Final da Estruturação

---
+++ classes/Consultorio.py
@@ -0,0 +1,16 @@
+class Consultorio:
+    def __init__(self, id):
+        self.id = id
+        self.filas = []
+    
+    def fechar_consultorio(self):
+        pass
+
+    def atender_outro_consultorio(x):
+        pass
+
+    def atender_prioritario(self):
+        pass
+
+    def atender(self):
+        pass
---
+++ classes/Floresta.py
@@ -0,0 +1,27 @@
+from classes.Heap import Heap
+
+class Floresta:
+    def __init__(self, paciente):
+        self.floresta = []
+        self.floresta.append(Heap(paciente))
+    
+    def adicionar(self, hp):
+        pass
+
+    def unir(self, floresta):
+        pass
+
+    def unir_caso1(x, y, z):
+        pass
+
+    def unir_caso2(x, y, z):
+        pass
+
+    def unir_caso3(x, y, z):
+        pass
+
+    def unir_caso4(x, y, z):
+        pass
+
+    def atender(self):
+        pass    
