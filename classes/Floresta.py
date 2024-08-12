b1b1bcebd85c137bc02d48d7e88472c4931bcc34
PabloKauan-TI <kpablo429@gmail.com>
2024-08-12 15:28:30 -0300

    heap binomial conluido junto de seus processos de união

--- classes/Floresta.py
+++ classes/Floresta.py
@@ -6,23 +6,37 @@ class Floresta:
         self.floresta.append(Heap(paciente))
     
     def unir(self, floresta = None):
-        pass
+        if floresta == None:
+            a = 0
+            x = a+1
+            y = a+2
+            while y <= (len(self.floresta)-1):
+                if self.floresta[a].grau == self.floresta[x].grau and self.floresta[x].grau != self.floresta[y].grau:
+                    self.unir_caso(a, x)
+                a+=1
+            self.unir_caso(a, x)
+        else:
+            for heap in floresta:
+                self.floresta.append(heap)
+            self.floresta.sort(key= lambda heap: heap.grau)
+            self.unir()
 
-    def unir_caso1(x, y, z):
-        pass
-
-    def unir_caso2(x, y, z):
-        pass
-
-    def unir_caso3(x, y, z):
-        pass
-
-    def unir_caso4(x, y, z):
-        pass
-
+    def unir_caso(self, x, y):
+        if self.floresta[x].prioridade <= self.floresta[y].prioridade:
+            self.floresta[x].filho.append(self.floresta[y])
+            self.floresta[x].filho.sort(key= lambda heap : heap.grau)
+            self.floresta[x].grau += 1
+            self.floresta.pop(y)
+        else:
+            self.floresta[y].filho.append(self.floresta[x])
+            self.floresta[y].filho.sort(key= lambda heap : heap.grau)
+            self.floresta[y].grau += 1
+            self.floresta.pop(x)
+    
     def adicionar(self, paciente):
         self.floresta.append(Heap(paciente))
         self.floresta.sort(key= lambda heap : heap.grau)
+        self.unir()
 
     def atender(self):
         pass    
